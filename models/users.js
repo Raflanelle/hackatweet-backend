b95278062fb0d23b731bccb325c5dd9b95d3ed2a
raphaelle <raflanelle@hotmail.com>
2024-07-20 20:50:31 +0200

    hackatweet backend avec loogin

--- models/users.js
+++ models/users.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
+//schéma avec sous- document
+
+//sous-document tweet
+		const tweetSchema = mongoose.Schema({
+			content: String,
+			hashtag: String,
+			creationDate: Date,
+		});
+
+//document principal user
 const usersSchema = mongoose.Schema({
     name: String,
     username: String,
     password: String,
     token: String,
+    tweet: tweetSchema,
    });
 
    const User = mongoose.model('users', usersSchema)
--- routes/tweets.js
+++ routes/tweets.js
@@ -1,53 +1,8 @@
 var express = require('express');
 var router = express.Router();
 require('../models/connection');
-const Tweet = require('../models/tweets');
 
-// GET all tweets
-router.get('/', (req, res) => {
-    Tweet.find()
-    .then(data => {
-        if (data) {
-        res.json({result: true, tweets: data})
-        }
-        else {
-        res.json({result: false});
-        }
-    })
-});
 
-//POST a new tweet
-router.post('/', (req, res) => {
-    const pattern = /#[a-z0-9]*/
-    const hashtag = req.body.content.match(pattern);
 
-    const newTweet = new Tweet ({
-        name: req.body.name,
-        username: req.body.username,
-        content: req.body.content,
-        hashtag: hashtag[0],
-        creationDate: new Date(),
-    })
-    
-    if (!req.body.content || req.body.content === '') {
-        res.json({result: false, error: 'Cannot post empty tweet !'})
-    }
-
-    else {
-        newTweet.save()
-        .then((data) => {
-            res.json({result: true, hashtag: newTweet.hashtag, tweet: data})
-            console.log(newTweet);
-        })
-    }
-})
-
-//DELETE a tweet
-router.delete('/:tweetId', (req, res) => {
-    Tweet.deleteOne({_id: req.params.tweetId})
-    .then(() => {
-        res.json({result: true})
-    })
-})
 
 module.exports = router;
